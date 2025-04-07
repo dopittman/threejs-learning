f40388e65214fd986911b90ff8fdfab4365067e5
David Pittman <53283971+dopittman@users.noreply.github.com>
2025-04-07 12:24:59 -0400

    feat: add scene swapping with GUI

--- threejs-boilerplate/src/main.ts
+++ threejs-boilerplate/src/main.ts
@@ -4,7 +4,42 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import Stats from "three/addons/libs/stats.module.js";
 import { GUI } from "dat.gui";
 
-const scene = new THREE.Scene();
+const sceneA = new THREE.Scene();
+sceneA.background = new THREE.Color(0x123456);
+// scene.background = new THREE.TextureLoader().load("https://sbcode.net/img/grid.png");
+sceneA.background = new THREE.CubeTextureLoader().setPath("https://sbcode.net/img/").load([
+  "px.jpg",
+  "nx.jpg",
+  "py.jpg",
+  "ny.jpg",
+  "pz.jpg",
+  "nz.jpg",
+]);
+sceneA.backgroundBlurriness = 0.02;
+
+
+const sceneB = new THREE.Scene();
+sceneB.background = new THREE.Color(0x123456);
+sceneB.backgroundBlurriness = 0.02;
+sceneB.background = new THREE.TextureLoader().load("https://sbcode.net/img/grid.png");
+
+const sceneC = new THREE.Scene();
+sceneC.background = new THREE.Color(0x123456);
+sceneC.backgroundBlurriness = 0.02;
+
+
+let activeScene = sceneA;
+const setScene = {
+  sceneA: function () {
+    activeScene = sceneA;
+  },
+  sceneB: function () {
+    activeScene = sceneB;
+  },
+  sceneC: function () {
+    activeScene = sceneC;
+  },
+};
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -33,9 +68,13 @@ const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshNormalMaterial({ wireframe: true });
 
 const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+sceneA.add(cube);
 
 const gui = new GUI();
+
+gui.add(setScene, "sceneA").name("Scene A");
+gui.add(setScene, "sceneB").name("Scene B");
+gui.add(setScene, "sceneC").name("Scene C");
 
 const cubeFolder = gui.addFolder("Cube");
 cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
@@ -58,7 +97,7 @@ function animate() {
   // cube.rotation.x += 0.01;
   // cube.rotation.y += 0.01;
 
-  renderer.render(scene, camera);
+  renderer.render(activeScene, camera);
 }
 
 animate();
